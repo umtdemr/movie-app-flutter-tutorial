77d5eca07a022f03e60c1b6e787237233063ef62
ümit <umitde296@gmail.com>
2020-10-23 17:24:08 +0300

    firt commit

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiContstants {
+  ApiContstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "155cb32fa2fd71b6952160c171fbad77";
+  static const String BASE_IMG_URL = "https://image.tmdb.org/t/p/w500";
+}
